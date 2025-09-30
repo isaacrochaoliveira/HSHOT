ddb25c00943ab909df4ade5a54857b880e95c2c2
isaacrochaoliveira <isaacvroliveira@gmail.com>
2025-09-30 16:59:24 -0300

    Update listar_versiculos_destacados.php

--- hshot/painel/processos_leituras/code/listar_versiculos_destacados.php
+++ hshot/painel/processos_leituras/code/listar_versiculos_destacados.php
@@ -18,24 +18,44 @@ $res = $sql->fetchAll(PDO::FETCH_ASSOC);
 if (count($res) == 0) {
     echo "<p class='arvo-regular'>Nenhum versículo destacado encontrado para este processo.</p>";
 } else {
-    $sql_pl = $pdo->query("SELECT * FROM processo_leitura WHERE id_pl = '$id_PL' AND IP_mem = '$_SESSION[IP_mem]'");
-    $res_pl = $sql_pl->fetchAll(PDO::FETCH_ASSOC);
-    $livro_n = $res_pl[0]['id_l'] ?? '';
+    ?>
+    <div class="d-flex flex-wrap gap-3">
+        <?php
+            for ($i = 0; $i < count($res); $i+=1) {
+                $sql_pl = $pdo->query("SELECT * FROM processo_leitura WHERE id_pl = '$id_PL' AND IP_mem = '$_SESSION[IP_mem]'");
+                $res_pl = $sql_pl->fetchAll(PDO::FETCH_ASSOC);
+                $livro_n = $res_pl[0]['id_l'] ?? '';
+            
+                $sql_livro = $pdo->query("SELECT * FROM livros WHERE id_l = '$livro_n'");
+                $res_livro = $sql_livro->fetchAll(PDO::FETCH_ASSOC);
+                $nome_livro = $res_livro[0]['nome_livro'] ?? 'Livro não encontrado';
+            
+                $capitulo = $res[$i]['cap_vd'] ?? '';
+                $versiculo = $res[$i]['vers_cs'] ?? '';
+                $versiculo_texto = $res[$i]['versiculo_ic'] ?? '';
+                $pensamento = $res[$i]['pensamento_ic'] ?? '';
 
-    $sql_livro = $pdo->query("SELECT * FROM livros WHERE id_l = '$livro_n'");
-    $res_livro = $sql_livro->fetchAll(PDO::FETCH_ASSOC);
-    $nome_livro = $res_livro[0]['nome_livro'] ?? 'Livro não encontrado';
-
-    $capitulo = $res[0]['cap_vd'] ?? '';
-    $versiculo = $res[0]['vers_cs'] ?? '';
-    $versiculo_texto = $res[0]['versiculo_ic'] ?? '';
-    $pensamento = $res[0]['pensamento_ic'] ?? '';
-?>
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title anton-regular"><?php echo $nome_livro ?> <?php echo $capitulo . ":". $versiculo?></h5>
-            <p class="card-text"><mark><?php echo $pensamento?></mark></p>
-        </div>
+                
+                ?>
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title anton-regular"><?php echo $nome_livro ?> <?php echo $capitulo . ":". $versiculo?></h5><br>
+                            <?php
+                                if (($pensamento == '') || ($pensamento == 'Versículo Destacado') || (($pensamento == 'Versículo destacado'))) {
+                                    ?>
+                                        <p class="card-text"><?php echo $versiculo_texto ?></p>
+                                    <?php
+                                } else {
+                                    ?>
+                                        <p class="card-text"><mark><?php echo $pensamento?></mark></p>
+                                    <?php
+                                }
+                                    ?>
+                        </div>
+                    </div>
+                <?php
+            }
+        ?>
     </div>
-<?php
+    <?php
 }
