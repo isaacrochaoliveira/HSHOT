728a7bda7b37dcd343d4329b440baa336d24243c
isaacrochaoliveira <isaacvroliveira@gmail.com>
2025-10-03 22:16:00 -0300

    Update login.php

--- hshot/painel/login.php
+++ hshot/painel/login.php
@@ -15,7 +15,7 @@
     </div>
 </div>
 
-<div class="d-flex flex-wrap justify-content-around">
+<div class="d-flex flex-wrap justify-content-around align-items-center">
     <div class="col-md-5">
         <div class="card card-outline card-success" style="padding: 0 50px;">
             <div class="card-header text-center">
@@ -42,14 +42,7 @@
                 <div class="col-4">
                     <button class="btn btn-success btn-block" id="btnLogin">ENTRAR</button>
                 </div>
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
-                </p>
-                <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new membership</a>
-                </p>
             </div>
-            <!-- /.card-body -->
         </div>
     </div>
     <div class="col-md-5">
@@ -59,56 +52,53 @@
             </div>
             <div class="card-body register-card-body">
                 <p class="login-box-msg">Registrar um novo membro</p>
-                <form action="../../index.html" method="post">
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Full name">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-user"></span>
-                            </div>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Nome Completo..." id="nome_mem">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
                         </div>
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" placeholder="Email" id="email_mem">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
                         </div>
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Senha" id="senha_mem">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
                         </div>
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Retype password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Redigite a senha" id="resenha_mem">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-                                <label for="agreeTerms">
-                                    I agree to the <a href="#">terms</a>
-                                </label>
-                            </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                            <label for="agreeTerms">
+                                I agree to the <a href="#">terms</a>
+                            </label>
                         </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Register</button>
-                        </div>
-                        <!-- /.col -->
                     </div>
-                </form>
-                <a href="login.html" class="text-center">I already have a membership</a>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block disabled" id="btnRegister">Register</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
             </div>
             <!-- /.form-box -->
         </div><!-- /.card -->
@@ -130,11 +120,34 @@
                 success: function(response) {
                     if (response.trim() == 'Sucesso!') {
                         location.href = 'http://localhost/HSHOT/hshot/home.php?pag=painel';
-                    } 
+                    }
                     $('#modalResponse').modal('show');
                     $('.mensagem').text(response)
                 }
             })
+        })
+    })
+</script>
+
+<script>
+    $(document).ready(function() {
+        $('#agreeTerms').click(function (){
+            if (document.getElementById('agreeTerms').checked) {
+                $('#btnRegister').removeClass('disabled');
+            } else {
+                $('#btnRegister').addClass('disabled');
+            }
+        })
+    })
+</script>
+
+<script>
+    $(document).ready(function() {
+        $("#btnRegister").click(function() {
+            var nome = $("#nome_mem").val();
+            var email = $("#email_mem").val();
+            var senha = $("#senha").val();
+            
         })
     })
 </script>
